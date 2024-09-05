dbe8948864b7b20332c99e3558075e91eaec4fe6
blayne-wesneski <102747184+blayne-wesneski@users.noreply.github.com>
2024-09-05 18:15:23 -0400

    Creation of random arrays of increasing lenghts and sorting.  -I don't know what I was trying to achieve with the math on that last commit  -Made a loop that actually generates arrays of the correct sizes  -Invoked bubble sort on all 22 random arrays

--- sortTiming.java
+++ sortTiming.java
@@ -5,65 +5,53 @@ public class sortTiming {
     public static void main(String[] args) {
 
         int[][] randomArrs = new int[22][];
+
+        /*
+         * I think I figured out the math on this loop. I'm not
+         * really sure what I was trying to achieve with my original math here. After I
+         * wrote it down and wasted an embarassing amount of scrap paper I got it
+         * figured out.I knew what I needed to do code wise, I just got caught on the
+         * math. I'm admittedly pretty bad at math.
+         */
         for (int i = 0; i < randomArrs.length; i++) {
 
-            if (i + 1 * 10 <= 10000) {
-                randomArrs[i] = randomArrs(1000, i + 1 * 10);
-            } else if (i + 1 * 10 > 10000) {
-                randomArrs[i] = randomArrs(1000, i * 10000);
+            if (i < 4) {
+                randomArrs[i] = randomArrs(1000, (int) Math.pow(10, i + 1));
+            } else if (i >= 4) {
+                randomArrs[i] = randomArrs(1000, ((i - 2) * 10000));
             }
 
         }
 
-        // TODO Fix this ridiculous mess. Probably use a 2D array I think? But how do I
-        // set the length of the arrays properly in that case? I wish I wasn't so bad at
-        // math.
-        int[] randomArr = copyArray(randomArrs[0]);
-        int[] randomArr2 = copyArray(randomArrs[1]);
-        int[] randomArr3 = copyArray(randomArrs[2]);
-        int[] randomArr4 = copyArray(randomArrs[3]);
-        int[] randomArr5 = copyArray(randomArrs[4]);
-        int[] randomArr6 = copyArray(randomArrs[5]);
-        int[] randomArr7 = copyArray(randomArrs[6]);
-        int[] randomArr8 = copyArray(randomArrs[7]);
-        int[] randomArr9 = copyArray(randomArrs[8]);
-        int[] randomArr10 = copyArray(randomArrs[9]);
-        int[] randomArr11 = copyArray(randomArrs[10]);
-        int[] randomArr12 = copyArray(randomArrs[11]);
-        int[] randomArr13 = copyArray(randomArrs[12]);
-        int[] randomArr14 = copyArray(randomArrs[13]);
-        int[] randomArr15 = copyArray(randomArrs[14]);
-        int[] randomArr16 = copyArray(randomArrs[15]);
-        int[] randomArr17 = copyArray(randomArrs[16]);
-        int[] randomArr18 = copyArray(randomArrs[17]);
-        int[] randomArr19 = copyArray(randomArrs[18]);
-        int[] randomArr20 = copyArray(randomArrs[19]);
-        int[] randomArr21 = copyArray(randomArrs[20]);
+        // copying the arrays for each sort function
+        int[][] randomArrs1 = new int[randomArrs.length][];
+        int[][] randomArrs2 = new int[randomArrs.length][];
+        int[][] randomArrs3 = new int[randomArrs.length][];
+        int[][] randomArrs4 = new int[randomArrs.length][];
+        int[][] randomArrs5 = new int[randomArrs.length][];
 
-        // Good lord it got worse. It definitely works... but it definitely sucks.
+        for (int i = 0; i < randomArrs.length; i++) {
+            randomArrs1[i] = copyArray(randomArrs[i]);
+            randomArrs2[i] = copyArray(randomArrs[i]);
+            randomArrs3[i] = copyArray(randomArrs[i]);
+            randomArrs4[i] = copyArray(randomArrs[i]);
+            randomArrs5[i] = copyArray(randomArrs[i]);
+        }
 
-        bubble(randomArr, 1, 22);
-        bubble(randomArr2, 2, 22);
-        bubble(randomArr3, 3, 22);
-        bubble(randomArr4, 4, 22);
-        bubble(randomArr5, 5, 22);
-        bubble(randomArr6, 6, 22);
-        bubble(randomArr7, 7, 22);
-        bubble(randomArr8, 8, 22);
-        bubble(randomArr9, 9, 22);
-        bubble(randomArr10, 10, 22);
-        bubble(randomArr11, 11, 22);
-        bubble(randomArr12, 12, 22);
-        bubble(randomArr13, 13, 22);
-        bubble(randomArr14, 14, 22);
-        bubble(randomArr15, 15, 22);
-        bubble(randomArr16, 16, 22);
-        bubble(randomArr17, 17, 22);
-        bubble(randomArr18, 18, 22);
-        bubble(randomArr19, 19, 22);
-        bubble(randomArr20, 20, 22);
-        bubble(randomArr21, 21, 22);
+        // invoke the sorts
+        for (int i = 0; i < randomArrs.length; i++) {
+            bubble(randomArrs[i], i + 1, randomArrs.length);
+        }
+    }
 
+    public static void printArray(int[] array) {
+        System.out.print("[");
+
+        for (int i = 0; i < array.length - 1; i++) {
+            System.out.print(array[i] + ", ");
+        }
+
+        System.out.println(array[array.length - 1] + "]");
     }
 
     // random array generation
